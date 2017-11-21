c72c4328c1dd15cd250273fdfdbad545f8ef0e34
tydaniel <tydaniel@gmail.com>
2017-11-21 16:19:54 +0800

    11/21 Checkin

--- vuejs-cinema/src/main.js
+++ vuejs-cinema/src/main.js
@@ -1,9 +1,43 @@
-// let msg = 'Hello';
-// let obj = {
-// 	msg
-// };
-
-// console.log(obj);
-
-
+import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
+
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
+new Vue({
+	el: "#app",
+	data: {
+		genre: [],
+		time: [],
+		movies: []
+	},
+	methods: {
+		checkFilter(category, title, checked){
+			// console.log(category, title, checked);
+			if(checked) {
+				this[category].push(title);				
+			} else {
+			    let index = this[category].indexOf(title);
+			    if(index > -1) {
+			    	this[category].splice(index, 1);
+			    } 
+			}
+		}
+	},
+	components: {
+		MovieList,
+		MovieFilter
+	},
+	created() {
+		//console.log(this.$http);
+		this.$http.get('/api').then(response => {
+			console.log(response.data);	
+			this.movies = response.data;
+		});
+	}
+});
+
